791e22f9d15f2827a60b1582aa6f7d5785b98aad
bluesmallpig <bluesmallpig@gmail.com>
2020-07-19 11:19:37 +0800

    Add files via upload

---
+++ Script/francis.js
@@ -0,0 +1,16 @@
+{
+	"id": "francis.app.sub",
+	"name": "francis应用订阅",
+	"author": "@francis",
+	"icon": "",
+	"repo": "https://github.com/bluesmallpig/Surge",
+	"apps": [{
+        id: 'txnews',
+        name: '腾讯新闻',
+        keys: ['sy_signurl_txnews', 'sy_cookie_txnews', 'video_txnews', 'sy_signurl_txnews2', 'sy_cookie_txnews2', 'video_txnews2'],
+        author: '@Francis',
+        repo: 'https://github.com/Sunert/Scripts/blob/master/Task/txnews.js',
+        icons: ['https://raw.githubusercontent.com/Orz-3/mini/master/txnews.png', 'https://raw.githubusercontent.com/Orz-3/task/master/txnews.png']
+		}
+	]
+}
